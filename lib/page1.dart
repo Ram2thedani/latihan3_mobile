f5bfcc06c14c91e486d3c5d8f3e772e0828969dc
ram2thedani <ram2thedani@gmail.com>
2024-08-20 11:44:37 +0700

    menambahkan page 1 dan page 2

---
+++ lib/page1.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.blue,
+            child: ElevatedButton(onPressed: () {}, child: Text("data")),
+          ),
+        ],
+      ),
+    );
+  }
+}
